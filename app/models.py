0f5011d7c1a4c5700e0574faa320138599353602
André Leão <andre.sp.leao@gmail.com>
2017-06-08 00:08:09 -0300

    Commit inicial com o models.py preenchido.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=False)
+    
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    movieid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200, blank=False)
+    genres = models.CharField(max_length=200, blank=False)
+    
+    def __str__(self):
+        return self.title
+class Rating(models.Model):
+    userid = models.ForeignKey(User, related_name='user_rating_fk')
+    movieid =models.ForeignKey(Movie, related_name='movie_rating_fk') 
+    rating = models.FloatField()
+
+    def __str__(self):
+        return str(self.userId)+" "+str(self.movieid)
