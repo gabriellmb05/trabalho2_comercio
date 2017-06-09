9c65c89b3c66765b0883c5e7a602a6717d2b9271
André Leão <andre.sp.leao@gmail.com>
2017-06-09 08:33:19 -0300

    Implementacao da view get_rating

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
+def get_rating(user_id,movie_id):
+	try:
+		rating = Rating.objects.get(userid=user_id,movieid=movie_id)
+		return rating.rating
+	except:
+		print ("Filme ou usuário inexistentes.")	
+	
 
-# Create your views here.
+
