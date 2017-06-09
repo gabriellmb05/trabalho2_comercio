66173289869513ead8b2d9408d5ebbcd77431a63
André Leão <andre.sp.leao@gmail.com>
2017-06-09 12:49:07 -0300

    try/except na funcao

--- app/views.py
+++ app/views.py
@@ -18,33 +18,36 @@ def get_avg_movie(user_id,set_of_movies):
 	return sum_ratings/len(set_of_movies)		
 
 def get_similarity(user_a,user_b):
-	user_a = User.objects.get(userid=user_a)
-	user_b = User.objects.get(userid=user_b)
+	try:
+		user_a = User.objects.get(userid=user_a)
+		user_b = User.objects.get(userid=user_b)
 
-	rated_movies_user_a = list(Rating.objects.filter(userid= user_a.userid)) 
-	rated_movies_user_b = list(Rating.objects.filter(userid= user_b.userid))
+		rated_movies_user_a = list(Rating.objects.filter(userid= user_a.userid)) 
+		rated_movies_user_b = list(Rating.objects.filter(userid= user_b.userid))
 
-	set_rated_movies_user_a = set(rating.movieid for rating in rated_movies_user_a) 
-	set_rated_movies_user_b = set(rating.movieid for rating in rated_movies_user_b) 	
+		set_rated_movies_user_a = set(rating.movieid for rating in rated_movies_user_a) 
+		set_rated_movies_user_b = set(rating.movieid for rating in rated_movies_user_b) 	
+		
+		intersection = set_rated_movies_user_a.intersection(set_rated_movies_user_b)
+
+		covariance = 0
+		deviation_a = 0
+		deviation_b = 0
+		for movie in intersection:
+			rating_a = get_rating(user_a,movie.movieid)
+			rating_b = get_rating(user_b,movie.movieid)
+			avg_rating_user_a = get_avg_movie(user_a,intersection)
+			avg_rating_user_b = get_avg_movie(user_b,intersection)
+
+			covariance += ((rating_a-avg_rating_user_a)*(rating_b-avg_rating_user_b))
+			deviation_a += pow((rating_a-avg_rating_user_a),2)
+			deviation_b += pow((rating_b-avg_rating_user_b),2)
+
+		similarity = covariance/sqrt(deviation_a)*sqrt(deviation_b)
+		return similarity
+	except:
+		print("Id do usuário inválida.")	
 	
-	intersection = set_rated_movies_user_a.intersection(set_rated_movies_user_b)
-
-	covariance = 0
-	deviation_a = 0
-	deviation_b = 0
-	for movie in intersection:
-		rating_a = get_rating(user_a,movie.movieid)
-		rating_b = get_rating(user_b,movie.movieid)
-		avg_rating_user_a = get_avg_movie(user_a,intersection)
-		avg_rating_user_b = get_avg_movie(user_b,intersection)
-
-		covariance += ((rating_a-avg_rating_user_a)*(rating_b-avg_rating_user_b))
-		deviation_a += pow((rating_a-avg_rating_user_a),2)
-		deviation_b += pow((rating_b-avg_rating_user_b),2)
-
-	similarity = covariance/sqrt(deviation_a)*sqrt(deviation_b)
-
-	return similarity
 
 	def similarity_test(request):
 		print(get_similarity(1,19))
