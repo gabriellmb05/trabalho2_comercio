1fbdb7de9080bca0785cea3649b385d159efea78
Gabriel <gabriel.luiz.mb@gmail.com>
2017-06-21 23:35:47 -0300

    acertado a identação

--- app/views.py
+++ app/views.py
@@ -41,46 +41,46 @@ def get_intersection(user_a,user_b):
     return intersection
 
 def get_similarity_pearson(user_a_id,user_b_id):
-    """Função responsável por calcular a similaridade entre dois usuários. Dado dois 2 id's de usuário a função retorna calcula	a
-    similaridade entre os mesmos aplicando a formula de pearson, com base nos filmes que ambos os usuários assistiram. A variável
-    covariancie acumula um somatório onde rating_a e rating_b são as classificações dos filmes que estão passando no loop e
-    avg_rating_user_a e avg_rating_user_b a média das clássificações dos filmes dos usuários a e b. A variável deviation_a e
-    deviation_b acumulam o desvio padrão dos usuários a e b. Caso a interceção seja vazia, significa que os usuários não possuem
-    similaridade logo recebem o valor -1. Caso o desvio padrão de a ou b seja zero, significa que o valor da classificação e da média
-    são iguais portanto tem similaridade 0 que é um valor neutro.
-    """
-    user_a = User.objects.get(userid=user_a_id)
-    user_b = User.objects.get(userid=user_b_id)
+	"""Função responsável por calcular a similaridade entre dois usuários. Dado dois  id's de usuário a função retorna calcula	a
+	similaridade entre os mesmos aplicando a formula de pearson, com base nos filmes que ambos os usuários assistiram. A variável
+	covariancie acumula um somatório onde rating_a e rating_b são as classificações dos filmes que estão passando no loop e
+	avg_rating_user_a e avg_rating_user_b a média das clássificações dos filmes dos usuários a e b. A variável deviation_a e
+	deviation_b acumulam o desvio padrão dos usuários a e b. Caso a interceção seja vazia, significa que os usuários não possuem
+	similaridade logo recebem o valor -1. Caso o desvio padrão de a ou b seja zero, significa que o valor da classificação e da média
+	são iguais portanto tem similaridade 0 que é um valor neutro.
+	"""
+	user_a = User.objects.get(userid=user_a_id)
+	user_b = User.objects.get(userid=user_b_id)
 
-    movies_a = Rating.objects.filter(userid=user_a)
-    movies_b = Rating.objects.filter(userid=user_b)
+	movies_a = Rating.objects.filter(userid=user_a)
+	movies_b = Rating.objects.filter(userid=user_b)
 
-    intersection = get_intersection(user_a,user_b)
+	intersection = get_intersection(user_a,user_b)
 
-    covariance = 0
-    deviation_a = 0
-    deviation_b = 0
+	covariance = 0
+	deviation_a = 0
+	deviation_b = 0
 
-    if(bool(intersection)==True):
-    	for movie in intersection:
-	            rating_a = get_rating(user_a,movie.movieid)
-	            rating_b = get_rating(user_b,movie.movieid)
-	            avg_rating_user_a = get_avg_movie(user_a,movies_a)
-	            avg_rating_user_b = get_avg_movie(user_b,movies_b)
-	            covariance += ((rating_a-avg_rating_user_a)*(rating_b-avg_rating_user_b))
-	            deviation_a += pow((rating_a-avg_rating_user_a),2)
-	            deviation_b += pow((rating_b-avg_rating_user_b),2)
+	if(bool(intersection)==True):
+		avg_rating_user_a = get_avg_movie(user_a,intersection)
+		avg_rating_user_b = get_avg_movie(user_b,intersection)
+		for movie in intersection:
+			rating_a = get_rating(user_a,movie.movieid)
+			rating_b = get_rating(user_b,movie.movieid)
+			covariance += ((rating_a-avg_rating_user_a)*(rating_b-avg_rating_user_b))
+			deviation_a += pow((rating_a-avg_rating_user_a),2)
+			deviation_b += pow((rating_b-avg_rating_user_b),2)
 
-	            if(deviation_a!=0 and deviation_b!=0):
-	                similarity = covariance/sqrt(deviation_a)*sqrt(deviation_b)
-	            else:
-	                similarity = 0
-    else:
-        similarity=-1
-    return similarity
+		if(deviation_a!=0 and deviation_b!=0):
+			similarity = covariance/(sqrt(deviation_a)*sqrt(deviation_b))
+		else:
+			similarity = 0
+	else:
+		similarity=-1
+	return similarity
 
 def get_similarity_cosine(user_a_id,user_b_id):
-    """Função responsável por calcular a similaridade entre dois usuários. Dado dois 2 id's de usuário a função retorna
+    """Função responsável por calcular a similaridade entre dois usuários. Dado dois id's de usuário a função retorna
     calcula a similaridade entre os mesmos aplicando a formula da distância do cosseno,	com base nos filmes que ambos os usuários
     assistiram. A variável rating_a e rating_b são vetores de com as classificações para os filmes que foram vistos pelo usuário.
     Caso a interceção seja vazia, significa que os usuários não possuem similaridade logo recebem o valor -1.
@@ -107,7 +107,9 @@ def get_similarity_cosine(user_a_id,user_b_id):
             abs_value_a += pow((rating_a[cont]),2)
             abs_value_b += pow((rating_b[cont]),2)
             cont+=1
-            similarity = (numerador)/(sqrt(abs_value_a) * sqrt(abs_value_b))
+        similarity = (numerador)/(sqrt(abs_value_a) * sqrt(abs_value_b))
+        print(rating_a)
+        print(rating_b)
     else:
         similarity=-1
     return similarity
@@ -234,7 +236,7 @@ def get_intersection_user(movie1, movie2):
 	return intersection
 
 def get_similarity_cosine_item(movie1, movie2):
-    """Função responsável por calcular a similaridade entre dois itens (Movie). Dado dois 2 objetos Movie a função retorna calcula
+    """Função responsável por calcular a similaridade entre dois itens (Movie). Dado  2 objetos Movie a função retorna calcula
     a similaridade entre os mesmos aplicando a formula da distância do cosseno,	com base nos filmes que ambos os usuários assistiram.
     A variável rating_a e rating_b são vetores de com as classificações para os filmes que foram vistos pelo usuário. Caso a
     interceção seja vazia, significa que os usuários não possuem similaridade logo recebem o valor -1.
@@ -260,33 +262,8 @@ def get_similarity_cosine_item(movie1, movie2):
             similarity=-1
         return similarity
 
-def get_prediction_item(user_id,movie_id):
-
-	user_a = User.objects.get(userid=user_id)
-	rating_differences = 0
-	similarities_sum = 0
-	if (Rating.objects.filter(userid=user_id,movieid=movie_id).exists()==False):
-		movie_ratings = Rating.objects.filter(movieid=movie_id)
-		for rating in movie_ratings:
-			user_b = User.objects.get(userid=rating.userid.userid)
-			movies_b = Rating.objects.filter(userid=user_b.userid)
-			similarity_a_b = get_similarity_cosine(user_a.userid,user_b.userid)
-			intersection = get_intersection(user_a,user_b)
-			avg_rating_user_b = get_avg_movie(user_b,movies_b)
-			rating_differences += (similarity_a_b * (get_rating(user_b.userid,movie_id)- avg_rating_user_b))
-			similarities_sum += similarity_a_b
-		movies_a = 	Rating.objects.filter(userid=user_a.userid)
-		avg_rating_user_a=get_avg_movie(user_a, movies_a)
-		if(similarities_sum !=0 ):
-			prediction = avg_rating_user_a + (rating_differences/similarities_sum)
-		else:
-		 	prediction = 0
-		return prediction
-	else:
-		print("O usuário já viu o filme escolhido, por favor escolha outro.")
-
 def get_intersection_user(movie1, movie2):
-    """Dado dois 2 objetos Movie a função retorna os
+    """Dado  2 objetos Movie a função retorna os
     um conjunto de id's de usuário classificaram os
     filmes passados por parametro.
     """
